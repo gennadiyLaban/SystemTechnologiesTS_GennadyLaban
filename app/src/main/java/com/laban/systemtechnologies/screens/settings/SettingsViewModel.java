889dadd979eb74bba264d7f410593cd5bbeddff6
genalaban <gena.laban@gmail.com>
2018-08-03 14:07:07 +0300

    create SettingsActivity.java create SettingsDataRepository.java create SettingsView.java create SettingsViewModel.java

---
+++ app/src/main/java/com/laban/systemtechnologies/screens/settings/SettingsViewModel.java
@@ -0,0 +1,16 @@
+package com.laban.systemtechnologies.screens.settings;
+
+import com.laban.systemtechnologies.screens.BaseViewModel;
+
+/**
+ * Copyright (c) 03.08.2018 Novacom.by
+ * All rights reserved
+ * Author: H.Laban
+ */
+public class SettingsViewModel extends BaseViewModel<SettingsDataRepository, SettingsView> {
+    public SettingsViewModel(SettingsDataRepository dataRepository, SettingsView view) {
+        super(dataRepository, view);
+    }
+
+
+}
