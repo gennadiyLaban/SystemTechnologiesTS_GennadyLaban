434344f4dafe884b0e31eb570b811d156dbaa198
genalaban <gena.laban@gmail.com>
2018-08-03 11:35:14 +0300

    create project

---
+++ app/src/main/java/com/laban/systemtechnologies/MainActivity.java
@@ -0,0 +1,15 @@
+package com.laban.systemtechnologies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.laban.systemtechnologies.com.systemtechnologiests_gennadylaban.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
