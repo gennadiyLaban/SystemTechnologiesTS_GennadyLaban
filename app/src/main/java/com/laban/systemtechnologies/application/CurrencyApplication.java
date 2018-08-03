de5e8d2398c0e061a905ea79b8303f3f812d21d3
genalaban <gena.laban@gmail.com>
2018-08-03 15:39:54 +0300

    create custom application

---
+++ app/src/main/java/com/laban/systemtechnologies/application/CurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.laban.systemtechnologies.application;
+
+import android.app.Application;
+
+public class CurrencyApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
