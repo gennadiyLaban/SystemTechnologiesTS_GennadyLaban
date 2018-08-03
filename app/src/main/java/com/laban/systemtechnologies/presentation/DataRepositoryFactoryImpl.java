812211ec22211a031fd3f36dd8ec636b3b961008
genalaban <gena.laban@gmail.com>
2018-08-03 16:47:14 +0300

    resolve stub for DataRepositoryFactoryImpl.java

--- app/src/main/java/com/laban/systemtechnologies/presentation/DataRepositoryFactoryImpl.java
+++ app/src/main/java/com/laban/systemtechnologies/presentation/DataRepositoryFactoryImpl.java
@@ -2,12 +2,30 @@ package com.laban.systemtechnologies.presentation;
 
 import com.laban.systemtechnologies.screens.DataRepository;
 import com.laban.systemtechnologies.screens.Screen;
+import com.laban.systemtechnologies.screens.currency.CurrencyDataRepository;
+import com.laban.systemtechnologies.screens.main.MainDataRepository;
+import com.laban.systemtechnologies.screens.settings.SettingsDataRepository;
 
 public class DataRepositoryFactoryImpl implements DataRepositoryFactory {
 
 
     @Override
     public <T extends DataRepository> T createRepository(Screen screen) {
-        return null;
+        DataRepository dataRepository = null;
+        switch (screen) {
+            case MAIN:
+                dataRepository = new MainDataRepository() {
+                };
+                break;
+            case CURRENCY_LIST:
+                dataRepository = new CurrencyDataRepository() {
+                };
+                break;
+            case SETTINGS:
+                dataRepository = new SettingsDataRepository() {
+                };
+                break;
+        }
+        return (T) dataRepository;
     }
 }
