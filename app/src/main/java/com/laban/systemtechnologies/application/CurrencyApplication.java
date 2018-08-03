d76456e1daf82371cd80ea73ade4f09d0e91fe41
genalaban <gena.laban@gmail.com>
2018-08-03 16:45:23 +0300

    add init VMFactory to CurrencyApplication.java, implements CurrencyApplication.java ViewModelFactory

--- app/src/main/java/com/laban/systemtechnologies/application/CurrencyApplication.java
+++ app/src/main/java/com/laban/systemtechnologies/application/CurrencyApplication.java
@@ -2,11 +2,29 @@ package com.laban.systemtechnologies.application;
 
 import android.app.Application;
 
-public class CurrencyApplication extends Application {
+import com.laban.systemtechnologies.presentation.DataRepositoryFactoryImpl;
+import com.laban.systemtechnologies.presentation.VMFactoryImpl;
+import com.laban.systemtechnologies.presentation.ViewModelFactory;
+import com.laban.systemtechnologies.screens.BaseViewModel;
+import com.laban.systemtechnologies.screens.Screen;
 
+public class CurrencyApplication extends Application implements ViewModelFactory {
+    private ViewModelFactory viewModelFactory;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        viewModelFactory = new VMFactoryImpl(new DataRepositoryFactoryImpl());
     }
+
+    @Override
+    public <T extends BaseViewModel> T onCreateScreen(Screen screen) {
+        return viewModelFactory.onCreateScreen(screen);
+    }
+
+    @Override
+    public void onDestroyScreen(Screen screen) {
+        viewModelFactory.onDestroyScreen(screen);
+    }
+
 }
